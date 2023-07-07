ffc9bdc98779e56371f33166c32f6dcce03dd8e5
eduardoadrian <104809718+eduardoadrian@users.noreply.github.com>
2023-07-07 17:00:15 -0300

    Conectamos a database y product.

--- app.py
+++ app.py
@@ -7,12 +7,27 @@ from email.message import EmailMessage
 from flask import (Flask, Response, jsonify, redirect, render_template,
                    request, url_for)
 
+import database as dbase
+from product import Product
+
+db = dbase.dbConnection()
+
 app = Flask(__name__)
 
 #Rutas de la aplicación
 @app.route('/')
 def home():
-    return render_template('index.html')
+    products = db['products']
+    productsReceived = products.find()
+    return render_template('index.html', products = productsReceived)
+
+'''#Ruta home 2
+@app.route('/')
+def homeDos():
+    products = db['products']
+    productsReceived = products.find()
+    return render_template('index.html', products = productsReceived)
+'''
 
 @app.errorhandler(404)
 def notFound(error=None):
@@ -23,6 +38,7 @@ def notFound(error=None):
     response = jsonify(message)
     response.status_code = 404
     return response
+
 
 
 if __name__ == '__main__':
